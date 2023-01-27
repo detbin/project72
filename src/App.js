361ab39c0478b35a7b138064937705411a00aea0
detbin <detbin@hotmail.com>
2023-01-27 22:32:21 +0100

    First commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import './componentes/ContactList'
+import ContactList from './componentes/ContactList';
+import Ejercicio101112 from './Ejercicio101112'
+
+function App() {
+  return (
+    <div>
+      <Ejercicio101112></Ejercicio101112>
+    </div>
+  );
+}
+export default App;
